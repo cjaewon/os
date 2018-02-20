6dde79029a964923c2994215ab013d871bb1925d
jewon <32125218+jaewon4492@users.noreply.github.com>
2018-02-20 14:13:02 +0900

    Update 무차별대입.py

--- 오픈소스/무차별대입.py
+++ 오픈소스/무차별대입.py
@@ -4,6 +4,10 @@ import random
 time.sleep(10)
 b = 250
 for i in range(250):
+        pyautogui.press('backenter')
+        pyautogui.press('backenter')
+        pyautogui.press('backenter')
+        pyautogui.press('backenter')
         time.sleep(0.3)
         b = b - 1
         pyautogui.typewrite(str(b))
